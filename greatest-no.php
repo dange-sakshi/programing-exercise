0a845eb3528b5802fbf9f65fdd3091ceb5f9d6c9
Aman Harde <aman_harde@vistasoftwares.com>
2023-06-08 14:14:41 +0530

    greatest no code completed

---
+++ greatest-no.php
@@ -0,0 +1,33 @@
+<?php
+      $num1=20;
+      $num2=15;
+      $num3=22;
+    //cond-3
+
+      if($num1>$num2 && $num1>$num3){
+        echo $num1;
+      }
+      elseif($num2 > $num1 && $num2 > $num3){
+             echo $num2;
+      }else{
+        echo $num3;
+      }
+
+      
+     //$num1 = 100 
+     //$num2 = 98   = $num1=100 = cond-1
+     //$num3 = 78
+      
+     //$num1 = 1000 
+     //$num2 = 2000  = $num3=3800 = cond-3
+     //$num3 = 3800
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
