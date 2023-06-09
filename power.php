f70051c663a3eda419df280d4e8e973f867e1b17
Aman Harde <aman_harde@vistasoftwares.com>
2023-06-09 15:08:11 +0530

    power of value code completed

---
+++ power.php
@@ -0,0 +1,31 @@
+<?php
+$num = 4;
+$power = 3;
+$result = 1;
+for($i=$power; $i>0; $i--){
+    $result = $result * $num;
+}
+echo $result;
+
+//$num = 5;
+//$power = 3;
+//i=3
+//3 > 0 yes
+//$result = 1 * 5 = 5
+//i = 2
+//2 > 0 yes
+//$result = 5 * 5 =25
+//i = 1
+//1 > 0
+//$result = 25 * 5 =125
+
+
+//$num=4;
+//$power=2;
+//i = 2
+//2 > 0
+//$result = 1 * 4 = 4
+//i = 1
+//1 > 0
+//$result = 4 *4 =16
+?>
