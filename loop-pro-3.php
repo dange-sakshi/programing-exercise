dd6f0a62924126900b8dda8d861a551f59344752
Sakshi Dange <sakshidange06@gmail.com>
2023-06-13 16:13:36 +0530

    loop-pro-3.php code modified

--- loop-pro-3.php
+++ loop-pro-3.php
@@ -1,8 +1,8 @@
 <?php
 //Write a program that prints this emoji: “😀” for every number divisible by 3, the “😔” emoji for every number divisible by 5, and the “😕” for every number divisible by both 3 and 5.
 //Do this up to 200.
-
-for($i = 1; $i <= 200; $i++){
+$emoji = readline('Enter Number : ');
+for($i = 1; $i <= $emoji; $i++){
     if ($i % 3 == 0 && $i % 5 == 0) {
         echo "😕"."<br>";
     }elseif($i % 5 == 0){
