e7527b2242cd89141bbf42c78658d559e4fbd035
Sakshi Dange <sakshidange06@gmail.com>
2023-06-14 17:24:14 +0530

    4b.php code modified

--- 4b.php
+++ 4b.php
@@ -1,4 +1,6 @@
 <?php
+//Write a program to calculate the overtime pay of an employee. Overtime is paid at the rate of Rs. 12.00 per hour for every hour worked above 40 hours. Assume that employees do not work for a fractional part of an hour. Take the number of hours worked by the employee separately as input.
+
 $num = readline('Enter hours: ');
 $emp = 1;
 $hour = 40;
