5796979e227cd4123e3f9090a76f1cafc90f33ca
Sakshi Dange <sakshidange06@gmail.com>
2023-06-17 14:06:18 +0530

    8b.php code modifide

--- 8b.php
+++ 8b.php
@@ -2,7 +2,8 @@
 //Write a program to print the largest of 5 numbers entered by the user.
 
 $large = 0;
-for($i = 1; $i<= 5; $i++){
+$count = 5;
+for($i = 1; $i<= $count; $i++){
     $num = readline('Enter number: ');
     if($num > $large){
         $large = $num;
@@ -11,6 +12,33 @@ for($i = 1; $i<= 5; $i++){
 }
 echo "$large is a  largest number";
 
-
+//$i = 1
+//1 <= $count true
+//$num = 1
+//1 > 0 true
+//$large = 1
+//$i = 2
+//2 <= $count true
+//$num = 2
+//2 > 1 true
+//$large = 2
+//$i = 3
+//3 <= $count true
+//$num = 3
+//3 > 2 true
+//$large = 3
+//$i = 4
+//4 <= $count true
+//$num = 4
+//4 > 3 
+//$large = 4
+//$i = 5
+// 5 <= $count true
+// $num = 5
+//5 > 4
+//$large = 5
+// $i = 6
+//6 <= 5 false
+//echo $large= 5
 
 ?>
