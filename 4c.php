460fd293df69e264f3c4bc8520fd4e05dc826e5c
Sakshi Dange <sakshidange06@gmail.com>
2023-06-19 17:25:40 +0530

    4c.php code completed

---
+++ 4c.php
@@ -0,0 +1,24 @@
+<?php
+//Write a program that first takes the following inputs:
+//- number of customers
+//- interest rate for a loan
+//Then for each customer up to the number of customers entered above, take as input the principal amount and duration in years and print total simple interest to be paid on the loan.
+$cust = 10;
+for ($i = 0; $i < $cust; $i++) {
+    $amount = readline('Enter amount: ');
+    $rate = readline('Enter rate: ');
+    $year = readline('Enter year: ');
+
+
+    $totalInt = $amount * $rate * $year /100;
+    echo $totalInt . PHP_EOL;
+
+
+    // $i = 0;
+    // 0 <= 10;
+    // $amount = 10000;
+    // $rate = 2;
+    // $year=2;
+    // $totalInt = 10000*2*2/100;
+    //  $totalInt = 400;
+}
