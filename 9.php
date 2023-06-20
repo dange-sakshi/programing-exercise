156ee2fd28a74a7cbc451d98ab1294ab652aa0fb
Sakshi Dange <sakshidange06@gmail.com>
2023-06-20 15:33:41 +0530

    9.php code completed

---
+++ 9.php
@@ -0,0 +1,21 @@
+<?php
+
+
+    //Write a program to print all prime numbers from 1 to 300. (Hint: Use nested loops, break and continue)
+
+   
+   for ($i = 1; $i <= 300; $i++){
+    $count = 0;
+    for ($j = 1; $j <= $i; $j++) {
+      
+        if (($i %  $j) == 0) {
+        
+            $count++;
+        }
+    }
+    if ($count < 3) {
+        echo $i . PHP_EOL;
+    }
+    
+}
+?>
