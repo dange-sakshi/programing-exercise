8a84866dfabd12c09098e7d74371b2066ca9efb0
Sakshi Dange <sakshidange06@gmail.com>
2023-06-22 15:35:50 +0530

    12.php code completed

--- 12.php
+++ 12.php
@@ -1,5 +1,7 @@
 <?php
-//
+//The distance between two cities (in km.) is input through the keyboard. Write a program to convert and print this distance in meters, feet, inches and centimeters.
+
+
 $km = readline('Enter km : ');
 
 $meter = $km * 1000;
