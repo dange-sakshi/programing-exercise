b96dcfbe25befe6cccdac7f5e57f1407280adb2d
Sakshi Dange <sakshidange06@gmail.com>
2023-06-26 12:21:04 +0530

    14.php code completed

---
+++ 14.php
@@ -0,0 +1,12 @@
+<?php
+//Temperature of a city in Fahrenheit degrees is input through the keyboard. Write a program to convert this temperature into Centigrade degrees.
+
+$fer = readline('Enter Temperature: ');
+$cent = ($fer - 32)* (5/9);
+echo $cent;
+
+
+
+
+
+?>
