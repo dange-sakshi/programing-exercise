7c76fd9f46146e806d23aa84ac6b54aba1f77196
Sakshi Dange <sakshidange06@gmail.com>
2023-06-29 14:26:00 +0530

    25.php code completed

---
+++ 25.php
@@ -0,0 +1,18 @@
+<?php
+//Any integer is input through the keyboard. Write a program to find out whether it is an odd number or even number.
+
+
+$num = readline('Enter number: ');
+
+if($num % 2 == 0){
+    echo "$num is a even number";
+}else{
+    echo "$num is a odd number"; 
+}
+
+
+
+
+
+
+?>
