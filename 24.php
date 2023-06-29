264841dc25d36f063fb5904f335ee1eb08d24f4f
Sakshi Dange <sakshidange06@gmail.com>
2023-06-29 12:01:39 +0530

    24.php code modified

--- 24.php
+++ 24.php
@@ -1,4 +1,7 @@
 <?php
+//If cost price and selling price of an item is input through the keyboard, write a program to determine whether the seller has made profit or incurred loss. Also determine how much profit he made or loss he incurred.
+
+
 $cost = readline('Enter Cost Price: ');
 $sell = readline('Enter Sell Price: ');
 
