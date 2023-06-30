d67d96b3bbc9de2e747b3be014a189007708056e
Sakshi Dange <sakshidange06@gmail.com>
2023-06-30 15:41:13 +0530

    30.php code completed

---
+++ 30.php
@@ -0,0 +1,10 @@
+<?php
+//Find the absolute value of a number entered through the keyboard
+$num = readline('Enter number: ');
+
+$abs = -1 * $num;
+echo "$abs is a absolute value of $num";
+
+
+
+?>
