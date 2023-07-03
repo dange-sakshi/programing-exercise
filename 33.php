dccf58dc592052ef226be42ab50c479000f6aa5c
Sakshi Dange <sakshidange06@gmail.com>
2023-07-03 16:59:49 +0530

    33.php code completed

--- 33.php
+++ 33.php
@@ -1,5 +1,7 @@
 <?php
-//
+//Given the coordinates (x, y) of a center of a circle and it’s radius, write a program which will determine whether a point lies inside the circle, on the circle or outside the circle. (Hint: Use sqrt( ) and pow( ) functions)
+
+
 $x1 = readline('Enter point x1: ');
 $y1 = readline('Enter point y1: ');
 $x2 = readline('Enter point x2: ');
