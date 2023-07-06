e6b2de119b8245db5c02226be2591ebb9999aecd
Sakshi Dange <sakshidange06@gmail.com>
2023-07-06 14:09:19 +0530

    4c.php code modifide

--- 4c.php
+++ 4c.php
@@ -4,20 +4,19 @@
 //- interest rate for a loan
 //Then for each customer up to the number of customers entered above, take as input the principal amount and duration in years and print total simple interest to be paid on the loan.
 $cust = readline('Enter no of cust: ');
+$rate = 20 / 100;
 for ($i = 0; $i < $cust; $i++) {
-    $amount = readline('Enter amount: ');
-    $rate = readline('Enter rate: ');
-    $year = readline('Enter duration in years: ');
+         $amount = readline('Enter amount: ');
+          // $rate = readline('Enter rate: ');
+          $year = readline('Enter duration in years: ');
+          $totalInt = $amount * $rate * $year;
+          echo $totalInt . PHP_EOL;
 
-
-    $totalInt = $amount * $rate * $year /100;
-    echo $totalInt . PHP_EOL;
-
-
+    // $rate = 20;
     // $i = 0;
     // 0 <= 10;
     // $amount = 10000;
-    // $rate = 2;
+
     // $year=2;
     // $totalInt = 10000*2*2/100;
     //  $totalInt = 400;
