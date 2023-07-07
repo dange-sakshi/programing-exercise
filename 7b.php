f6940c819c498f776cba299c8174d6201dae7d44
Sakshi Dange <sakshidange06@gmail.com>
2023-07-07 12:11:36 +0530

    7b.php code modifide

--- 7b.php
+++ 7b.php
@@ -2,26 +2,35 @@
 //Given a positive integer with an arbitrary number of digits, write a program to determine whether it is an Armstrong number. 
 $num = readline('Enter number: ');
 $qu = $num;
-$count = strlen(strval($qu));
+$qu1 = $num;
 $sum = 0;
+$count =  0;
+while ((int) $qu > 0) {
+    $r = (int)$qu % 10;
+    $count++;
+    $qu = (int)$qu / 10;
+}
+$count1 = $count;
 
-while($qu != 0){
-   
-    $rem = (int)$qu % 10;
+
+while ($num != 0) {
+
+    $rem = (int)$num % 10;
+
     $mult = 1;
-      for($i=1; $i <= $count; $i++){
+    for ($i = 1; $i <= $count; $i++) {
         $mult *= $rem;
     }
-    
-          $sum += $mult;
-          $qu = $qu / 10;
-}
-if($sum == $num){
-    echo "$num is a  Armstrong number";
-}else{
-    echo "$num is not a  Armstrong number";
+
+    $sum += $mult;
+
+    $num = $num / 10;
+    // echo $sum;
 }
 
-
-
+if ($sum == $qu1) {
+    echo "$qu1 is an  Armstrong number";
+} else {
+    echo "$qu1 is not an  Armstrong number";
+}
 ?>
