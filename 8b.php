530d150c5c22b4756da2a2cc7a1eee0c42b56abb
Sakshi Dange <sakshidange06@gmail.com>
2023-07-12 13:47:39 +0530

    8b.php code modifide

--- 8b.php
+++ 8b.php
@@ -5,6 +5,9 @@ $large = 0;
 $count = 5;
 for($i = 1; $i<= $count; $i++){
     $num = readline('Enter number: ');
+    if($i == 1){
+        $large = $num;
+    }
     if($num > $large){
         $large = $num;
     }
@@ -12,33 +15,6 @@ for($i = 1; $i<= $count; $i++){
 }
 echo "$large is a  largest number";
 
-//$i = 1
-//1 <= $count true
-//$num = 1
-//1 > 0 true
-//$large = 1
-//$i = 2
-//2 <= $count true
-//$num = 2
-//2 > 1 true
-//$large = 2
-//$i = 3
-//3 <= $count true
-//$num = 3
-//3 > 2 true
-//$large = 3
-//$i = 4
-//4 <= $count true
-//$num = 4
-//4 > 3 
-//$large = 4
-//$i = 5
-// 5 <= $count true
-// $num = 5
-//5 > 4
-//$large = 5
-// $i = 6
-//6 <= 5 false
-//echo $large= 5
+
 
 ?>
