dc6158b3300db7383b9eb59e11dd6e4ca0907450
Sakshi Dange <sakshidange06@gmail.com>
2023-08-23 15:54:06 +0530

    15.php code completed

--- 15.php
+++ 15.php
@@ -2,18 +2,17 @@
 //The length & breadth of a rectangle and radius of a circle are input through the keyboard. Write a program to calculate the area & perimeter of the rectangle, and the area & circumference of the circle.
 
 
- $length = readline('Enter length: ');
- $breadth = readline('Enter breadth: ');
- $rA = $length * $breadth;
- $pr = 2*($length + $breadth);
+$length = readline('Enter length: ') . PHP_EOL;
+$breadth = readline('Enter breadth: ') . PHP_EOL;
+$radius = readline('Enter radius: ') . PHP_EOL;
+$rectangle_area = $length * $breadth;
+$perimeter = 2 * ($length + $breadth);
 
-$radius = readline('Enter radius: ');
- $cA = 3.14 * $radius*$radius;
- $cc = 2*3.14*$radius;
+$circle_area = 3.14 * $radius * $radius;
+$circumference = 2 * 3.14 * $radius;
 
-echo "$rA Area of Rectangle" . PHP_EOL;
-echo "$pr Perimeter of the Rectangle"  . PHP_EOL;
-echo "$cA Area of the circle" . PHP_EOL ;
-echo "$cc circumference of the circle." . PHP_EOL;
-
+echo "$rectangle_area Area of Rectangle" . PHP_EOL;
+echo "$perimeter Perimeter of the Rectangle"  . PHP_EOL;
+echo "$circle_area Area of the circle" . PHP_EOL;
+echo "$circumference circumference of the circle." . PHP_EOL;
 ?>
