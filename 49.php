f4aa99f017bac999fae92ceec512947da950ef67
Sakshi Dange <sakshidange06@gmail.com>
2023-09-06 15:40:36 +0530

    49.php code completed

---
+++ 49.php
@@ -0,0 +1,10 @@
+<?php
+//Write a program to fill the entire screen with a smiling face, forever until the user terminates the program.
+
+while(true){
+    //    echo "😀";
+   echo "\xF0\x9F\x98\x80";
+
+}
+
+?>
