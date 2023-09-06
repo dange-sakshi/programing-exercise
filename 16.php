d427d1579db4e2f3a3d4185c297b3b681140adb9
Sakshi Dange <sakshidange06@gmail.com>
2023-09-06 13:07:32 +0530

    16.php code completed

--- 16.php
+++ 16.php
@@ -1,4 +1,7 @@
 <?php
+//Two numbers are input through the keyboard into two locations C and D. Write a program to interchange the contents of C and D.
+
+
 $c = readline('Enter the value of c: ');
 $d = readline('Enter the value of d: ');
 
